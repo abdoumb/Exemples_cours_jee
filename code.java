07360375cf7ea2577ea64667fa1e545e703fa280
abdoumb <abdelilah_oujda@hotmail.com>
2016-11-28 23:08:51 +0000

    ajouter du text

--- code.java
+++ code.java
@@ -2,6 +2,7 @@ public class Code {
 	public static void main(String[] args) {
 		System.out.println("du code java");
 		System.out.println("c'est cool");
+		System.out.println("et si tu clone ceci");
 	}
 }
 
